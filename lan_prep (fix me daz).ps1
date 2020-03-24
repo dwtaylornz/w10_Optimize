458bb04623e7cb1c6a94930aa2091bf700b98357
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-03-24 13:04:19 +1300

    Update lan_prep (fix me daz).ps1

--- lan_prep (fix me daz).ps1
+++ lan_prep (fix me daz).ps1
@@ -1,3 +1,36 @@
+# Services to set to "below normal" priority
+$services_belownormal = @(
+	"SearchUI.exe"
+	"SearchIndexer.exe"
+        )
+
+# Services to set to "low" priority
+$services_low = @(
+	"nessusd.exe"
+	"SavService.exe"
+	"swi_fc.exe"
+	"CcmExec.exe"	
+	"DSATray.exe"
+	"esrv_svc.exe"
+	"WMiPrvSE.exe"
+	"fontdrvhost.exe"
+	"RuntimeBroker.exe"
+	"DSATray.exe"
+	"MsMpEng.exe"
+        )
+
+# Services to disable
+$services_disable = @(
+        "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
+        "DiagTrack"                                # Diagnostics Tracking Service
+        "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
+        "XblAuthManager"                           # Xbox Live Auth Manager
+        "XblGameSave"                              # Xbox Live Game Save Service
+        "XboxNetApiSvc"                            # Xbox Live Networking Service
+        "XboxGipSvc"
+        "ndu"                                      # Windows Network Data Usage Monitor
+        )
+
 function Show-Menu {
     Clear-Host
     Write-Host "================ LAN Optimize ================"
@@ -11,122 +44,73 @@ function Show-Menu {
     Write-Host ""
     Write-Host "  - All Above (in option 1)"
     Write-Host "  - Disable Game Bar and Recording services"
-    Write-Host "  - Deprioritize services"
-    Write-Host "  - Disable xBox services"
+    Write-Host "  - Deprioritize services (temp, does not hold over reboot)"
+    Write-Host "  - Disable xBox services (perminant)"
     Write-Host ""
     Write-Host "=============================================="
     Write-Host ""
     Write-Host "Press '3' to set power profile to Balanced (for home)"
     Write-Host ""
     Write-Host ""
-
 }
 
-function Run-One {
+function Run-Quick {
 	# if exists changes power mode to "high performance" -- forces higher clock speed on computer 
-	
 	powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c | Out-Null
 	powercfg.exe /setactive bbc1d9f4-fcd1-46b8-b78e-18ca71c442c2 | Out-Null
 	Write-Host ""
 	Write-Host "Your Power Profile is now set to : " -NoNewLine
 	powercfg.exe /getactivescheme
 
-
-    Write-Host "Set DNS back to AUTO on DHCP (for LAN cache)..." 
-    Get-NetAdapter | Set-DnsClientServerAddress -ResetServerAddresses
+	Write-Host "Set DNS back to AUTO on DHCP (for LAN cache)..." 
+    	Get-NetAdapter | Set-DnsClientServerAddress -ResetServerAddresses
     	Write-Host ""
 }
 
-function Run-Two {
+function Run-Works {
 
     Write-Host ""
     Write-Host "Remove W10 xbox app..." 
     Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
-    #REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GameDVR /v AllowgameDVR /t REG_DWORD /d 0
-    #REG ADD HKEY_CURRENT_USER\Software\Microsoft\GameBar /v AllowAutoGameMode /t REG_DWORD /d 0
-    #Get-AppxPackage *xbox* | Remove-AppxPackage
-    #Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowgameDVR" "0"
-    #New-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowgameDVR" "0"
-
+    
     Write-Output "Disable easy access keyboard stuff..."
     Set-ItemProperty "HKCU:\Control Panel\Accessibility\StickyKeys" "Flags" "506"
     Set-ItemProperty "HKCU:\Control Panel\Accessibility\Keyboard Response" "Flags" "122"
     Set-ItemProperty "HKCU:\Control Panel\Accessibility\ToggleKeys" "Flags" "58"
     
-    # Reduce Prioitity of services to speed up my PC 
-
-    # Services to set to "below normal" priority
-    $services_belownormal = @(
-	    "SearchUI.exe"
-	    "SearchIndexer.exe"
-        )
-
-    # Services to set to "low" priority
-    $services_belownormal = @(
-	    "nessusd.exe"
-	    "SavService.exe"
-	    "swi_fc.exe"
-	    "CcmExec.exe"	
-	    "DSATray.exe"
-	    "esrv_svc.exe"
-	    "WMiPrvSE.exe"
-	    "fontdrvhost.exe"
-	    "RuntimeBroker.exe"
-	    "DSATray.exe"
-        )
-
-    # Services to disable
-    $services_disable = @(
-        "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
-        "DiagTrack"                                # Diagnostics Tracking Service
-        "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
-        "XblAuthManager"                           # Xbox Live Auth Manager
-        "XblGameSave"                              # Xbox Live Game Save Service
-        "XboxNetApiSvc"                            # Xbox Live Networking Service
-        "ndu"                                      # Windows Network Data Usage Monitor
-        )
-
+    # Reduce Prioitity of services to speed up my PC  
     Write-Output "De-prioritising Services - Below Normal ..."
     foreach ($service in $services_belownormal) {
 
         Write-Host "  Adjusting $service" 
 	    Get-WmiObject Win32_process -filter "name = '$service'" | foreach-object { $_.SetPriority(16384) } | Out-Null
-
 	    }
 
     Write-Output "De-prioritising Services - Low  ..."
-    foreach ($service in $services_belownormal) {
+    foreach ($service in $services_low) {
 
 	    Write-Host "  Adjusting $service" 
 	    Get-WmiObject Win32_process -filter "name = '$service'" | foreach-object { $_.SetPriority(64) } | Out-Null
-
 	    }
-
-    #256	Realtime
-    #128	High
-    #32768	Above normal
-    #32		Normal
-    #16384	Below normal
-    #64		Low
 
     Write-Output "Disabling Services..." 
     foreach ($service in $services_disable) {
     
-    Write-Output "  Disabling $service"
-    Get-Service -Name $service | Set-Service -StartupType Disabled
-}
+    	Write-Output "  Disabling $service"
+    	Get-Service -Name $service | Set-Service -StartupType Disabled
+	}
 
     Write-Output ""
 }
 
-function Run-Three {
+function Run-Balanced {
 	# change power profile back to balanced 
 	
 	powercfg.exe /setactive 381b4222-f694-41f0-9685-ff5bb260df2e | Out-Null
 	Write-Host ""
 	Write-Host "Your Power Profile is now set to : " -NoNewLine
-    powercfg.exe /getactivescheme
-    Write-Host ""
+    	powercfg.exe /getactivescheme
+	Write-Host ""
 }
 
 do
@@ -135,16 +119,13 @@ do
     $selection = Read-Host "(Q to quit) "
     switch ($selection)
     {
-    '1' {Run-One} 
+    '1' {Run-Quick} 
     '2' {    
-        Run-One
-        Run-Two
+        Run-Quick
+        Run-Works
         } 
-    '3' {Run-Three}
+    '3' {Run-Balanced}
     }
     pause
  }
  until ($selection -eq 'q')
-
-
- # todo 
